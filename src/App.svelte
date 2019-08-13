65d5c62ba2236801b04fa2ed9c3b2e01ea429bde
Manfred Headley <manfredheadley@pop-os.localdomain>
2019-08-12 21:14:21 -0400

    some comments

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
 <script>
-	import Sidebar from './Sidebar.svelte'
 	import Home from './Home.svelte'
 	import Author from './Author.svelte'
 	import Book from './Book.svelte'
+	import Sidebar from './Sidebar.svelte'
 	import Router from 'svelte-spa-router'
 
 	const routes = {
@@ -10,9 +10,30 @@
 		'/author/': Author,
 		'/book/': Book,
 	}
+
 </script>
 
 <Sidebar />
+
 <main>
 	<Router {routes}/>
 </main>
+
+<!-- 
+ Without the router included no error is thrown. Comment out about and uncomment this to see the test running with `npm run test`
+<script>
+		import Home from './Home.svelte'
+		import Author from './Author.svelte'
+		import Book from './Book.svelte'
+		
+		const routes = {
+			'/': Home,
+			'/author/': Author,
+			'/book/': Book,
+		}
+	
+	</script>
+		
+	<main>
+		<h1>Hello There!</h1>
+	</main> -->
